55ea1fc7950da27752e17bd195ce30183a94cece
Mahnoor-Akhter <60778404+Mahnoor-Akhter@users.noreply.github.com>
2022-01-31 22:42:30 +0000

    final test screen shot

--- src/app/strings/greet.spec.ts
+++ src/app/strings/greet.spec.ts
@@ -1,6 +1,6 @@
 import { greet } from "./greet";
 
-describe('greet',() =>
+describe('greet Mah Noor S00199547',() =>
 {
     it('it should include the name in the message',()=>{
         expect(greet('Mah Noor')).toContain('Mah Noor');
